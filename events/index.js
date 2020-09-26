aca14fc51a99d83a254f45e3d053dbcd77a4a636
Eyituoyo George Ikomi <etikoms@gmail.com>
2020-09-26 11:16:56 +0100

    events

---
+++ events/index.js
@@ -0,0 +1,60 @@
+// const post = document.querySelector(".post");
+// post.addEventListener("click", (event) => {
+//     console.log(event.target);
+// });
+
+// const post = document.querySelector(".post");
+// post.addEventListener("click", (event) => {
+//     alert("Do you want to edit this post");
+// });
+
+// const posts = document.querySelectorAll(".post");
+// posts.forEach((post) => {
+//     post.addEventListener("click", (event) => {
+//         alert("Do you want to edit this post");
+//     });
+// });
+
+// document.body.addEventListener("click", (event) => {
+//     if (!event.target.matches(".post")) {
+//         return;
+//     }
+
+//     alert("Do you want to edit this post");
+// });
+
+// document.body.addEventListener("click", (event) => {
+//     if (!event.target.closest(".post")) {
+//         return;
+//     }
+
+//     alert("Do you want to edit this post");
+// });
+
+// document.body.addEventListener("mouseover", (event) => {
+//     if (!event.target.closest) {
+//         return;
+//     }
+
+//     alert("Do you want to edit this post");
+// });
+
+// document.body.addEventListener("mouseout", (event) => {
+//     if (!event.target.closest) {
+//         return;
+//     }
+
+//     alert("Do you want to edit this post");
+// });
+
+// document.body.addEventListener("keyup", (event) => {
+//     alert("Do you want to edit this post");
+// });
+
+// document.body.addEventListener("keydown", (event) => {
+//     alert("Do you want to edit this post");
+// });
+
+document.body.addEventListener("keypress", (event) => {
+    alert("Do you want to edit this post");
+});
