71b92b855e6eb5a8058e6f4064deb849a592c8d0
Eyituoyo George Ikomi <etikoms@gmail.com>
2020-09-26 07:06:38 +0100

    getting single/multiple elements

--- get-single-&_multiple-elements/index.js
+++ get-single-&_multiple-elements/index.js
@@ -1,20 +1,20 @@
-// const home = document.getElementById("home");
-// console.log(home);
+const home = document.getElementById("home");
+console.log(home);
 
-// const links = document.querySelectorAll("a");
-// console.log(links);
-// links.forEach((link) => console.log(link));
+const links = document.querySelectorAll("a");
+console.log(links);
+links.forEach((link) => console.log(link));
 
-// const link = document.querySelector("a");
-// console.log(link);
+const link = document.querySelector("a");
+console.log(link);
 
-// const links = document.querySelectorAll("a");
-// links.forEach((link) => {
-//     if (link.matches('a[href = "/login"]')) {
-//         const loginLink = link;
-//         console.log(loginLink);
-//     }
-// });
+const links = document.querySelectorAll("a");
+links.forEach((link) => {
+    if (link.matches('a[href = "/login"]')) {
+        const loginLink = link;
+        console.log(loginLink);
+    }
+});
 
 const divs = document.getElementsByTagName("div");
 console.log(divs);
