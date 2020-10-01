438c512893cef3b8b59b93c45f018c421f98c562
Eyituoyo George Ikomi <etikoms@gmail.com>
2020-10-01 07:10:36 +0100

    avoid mutations with array spread

---
+++ arrays/avoid-mutations-with-array-spread/index.js
@@ -0,0 +1,28 @@
+/*
+const lunchMenuIdeas = ["Harvest Salad", "Southern Fried Chicken"];
+console.log(lunchMenuIdeas);
+
+const allMenuIdeas = lunchMenuIdeas;
+console.log(allMenuIdeas);
+
+allMenuIdeas.push("Club Sandwich");
+console.log(lunchMenuIdeas);
+console.log(allMenuIdeas);
+*/
+
+/*
+const lunchMenuIdeas = ["Harvest Salad", "Southern Fried Chicken"];
+console.log(lunchMenuIdeas);
+
+const allMenuIdeas = lunchMenuIdeas.concat("Club Sandwich");
+console.log(allMenuIdeas);
+*/
+
+const lunchMenuIdeas = ["Harvest Salad", "Southern Fried Chicken"];
+console.log(lunchMenuIdeas);
+
+const allMenuIdeas = [...lunchMenuIdeas];
+console.log(allMenuIdeas);
+
+allMenuIdeas.push("Club Sandwich");
+console.log(allMenuIdeas);
