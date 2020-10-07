2e75d9af6ff17a753fdceec8269cf60ed3bafbc5
Eyituoyo George Ikomi <etikoms@gmail.com>
2020-10-07 14:09:12 +0100

    practice - constructor function

--- classes/practice/index.js
+++ classes/practice/index.js
@@ -9,6 +9,7 @@ const student1 = {
 };
 */
 
+/*
 function Student(id, name, subjects = []) {
     this.id = id;
     this.name = name;
@@ -29,3 +30,24 @@ student2.addSubject("Chemistry");
 
 console.log(student1);
 console.log(student2);
+*/
+
+function Book(id, title, author, themes = []) {
+    this.id = id;
+    this.title = title;
+    this.author = author;
+    this.themes = themes;
+}
+
+const favouriteBook1 = new Book(1, "Village Headmaster", "NTA");
+const favouriteBook2 = new Book(2, "Cock crows at dawn", "Chris Okotie");
+
+Book.prototype.addTheme = function(theme) {
+    this.themes = [...this.themes, theme];
+};
+
+favouriteBook1.addTheme("Rising star");
+favouriteBook2.addTheme("Niagara falls");
+
+console.log(favouriteBook1);
+console.log(favouriteBook2);
